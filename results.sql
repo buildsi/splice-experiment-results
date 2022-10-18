bd99c2f860d2d1064b71381e328456bffe14ec7f
Tim Haines <thaines.astro@gmail.com>
2022-10-18 18:30:51 -0500

    Add sql script to create database

---
+++ results.sql
@@ -0,0 +1,14 @@
+CREATE TABLE results("a" text, "b" text, "original" text, "changed" text, "analysis" text, "time" float, "predictor" text, "prediction" text);
+.mode csv
+.import results.csv results
+
+-- 'Unknown' results are linker script files
+delete from results where original in(select distinct original from results where prediction="Unknown");
+
+-- 'two_predictors' contains all results except those from abi-laboratory
+CREATE TABLE two_predictors("a" text, "b" text, "original" text, "changed" text, "analysis" text, "time" float, "predictor" text, "prediction" text);
+insert into two_predictors select * from results where predictor<>'abi-laboratory';
+
+-- 'three_predictors' contains all results from predictors except those libraries where abi-lab crashed
+CREATE TABLE three_predictors("a" text, "b" text, "original" text, "changed" text, "analysis" text, "time" float, "predictor" text, "prediction" text);
+insert into three_predictors select * from results where original not in(select distinct original from results where prediction='Terminated');
